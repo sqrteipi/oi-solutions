1c998cf249e74e4eb887e2f564ffd12e89307b37
sqrteipi <137189595+sqrteipi@users.noreply.github.com>
2023-11-07 17:17:46 +0800

    Create CF1367A.cpp

---
+++ CF1367A.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+  int t;
+  cin >> t;
+  while (t--) {
+    string s;
+    cin >> s;
+    for (int i=0; i<s.length()-1; i+=2){
+      cout << s[i];
+    }
+    cout << s.back() << "\n";
+  }
+}
