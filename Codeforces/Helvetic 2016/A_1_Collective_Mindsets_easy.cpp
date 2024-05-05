7abced3698b6db5b2340ccb755ae0f32669084b1
sqrteipi <wongcheukiu@gmail.com>
2024-05-06 00:04:36 +0800

    Uploaded AC A1 Code

--- Codeforces/Helvetic 2016/A_1_Collective_Mindsets_easy.cpp
+++ Codeforces/Helvetic 2016/A_1_Collective_Mindsets_easy.cpp
@@ -9,5 +9,7 @@ Brainstorm:
 
 If n = 1, obviously 1
 If n = 2, still 1 (since always win vote)
-If n = 3, requires 2
+If n = 3, requires 2:
+The 3rd people obviously know he will get no brain if N = 2
+
 */
